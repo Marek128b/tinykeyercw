d9f34b0c6b1acfce456248b70abf1f262a6e64b9
Georg Marek <georg.marek687@gmail.com>
2024-10-31 09:42:17 +0100

    added somewhat better working version

--- tinykeyercw_code/src/main.cpp
+++ tinykeyercw_code/src/main.cpp
@@ -6,12 +6,16 @@ int led_pin = PIN_PA2;
 int buzzer_pin = PIN_PA1;
 
 int interval_time = 500;
-
+int freq_buzz = 1000;
+int interval_time_buzz = 1000;
 unsigned long long last_millis = 0;
 boolean buzzerState = false;
+unsigned long long last_micros_buzz = 0;
+boolean buzzerStatePWM = false;
 
 void setup()
 {
+  interval_time_buzz = (float)(1 / (float)freq_buzz) * 1000000;
   pinMode(led_pin, OUTPUT);
   pinMode(buzzer_pin, OUTPUT);
   pinMode(dit_pin, INPUT);
@@ -25,7 +29,6 @@ void loop()
     if (millis() - last_millis >= interval_time / 3)
     {
       buzzerState = !buzzerState;
-      digitalWrite(led_pin, buzzerState);
       last_millis = millis();
     }
   }
@@ -35,19 +38,29 @@ void loop()
     if (millis() - last_millis >= interval_time)
     {
       buzzerState = !buzzerState;
-      digitalWrite(led_pin, buzzerState);
       last_millis = millis();
     }
   }
 
-/*if (buzzerState)
+  if (buzzerState)
   {
-    tone(buzzer_pin, 1000);
+
+    digitalWrite(led_pin, HIGH);
+    if (micros() - last_micros_buzz >= interval_time_buzz)
+    {
+      buzzerStatePWM = !buzzerStatePWM;
+      digitalWrite(buzzer_pin, buzzerStatePWM);
+      last_micros_buzz = micros();
+    }
   }
   else
   {
-    noTone(buzzer_pin);
+    digitalWrite(buzzer_pin, LOW);
+    digitalWrite(led_pin, LOW);
   }
-*/
-  
+
+  if (digitalRead(dit_pin) && digitalRead(dah_pin))
+  {
+    buzzerState = false;
+  }
 }
